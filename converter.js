0ecd0e23ea014d8a93a4f7c0d0f01585fd8cec43
Senanyo Benwari <65190458+NightlyHawk@users.noreply.github.com>
2022-08-10 20:12:14 +0100

    Update converter.js

--- converter.js
+++ converter.js
@@ -160,9 +160,9 @@ dwn_btn.onclick = function() {
         base64image2.style.height = '32px'
         base64image2.style.width = '32px'    
         base64image2.src = canvas.toDataURL("image/png");
-        base64image2.title = "favicon2"
+        this.base64image2.id = "favicon2"
         function toDataURL(src, callback){
-            if(base64image2.title === "favicon2"){
+            if(base64image2.id === "favicon2"){
                 base64image2.onload = function(){
                 var context = canvas.getcontext('2d');
                 canvas.height = this.naturalHeight;
